c260bc1181dc4c4fc8fde591149a635f7c30aa76
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2024-09-26 12:00:29 +0300

    ready hw-12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,32 @@
+import axios from 'axios';
 export const BASE_URL = "https://pixabay.com/";
 export const API_KEY = "46035162-73af77dc9f391d36c3be30780";
 
+export let pageNumber = 1;
+export let perPage = 15; 
 
-export function queryFunction (query) {
+
+export async function queryFunction (query, pageNumber) {
 const options = {
   key: API_KEY,
   q: query,
   image_type: "photo",
   orientation: "horizontal",
   safesearch: "true",
+  page: pageNumber,
+  per_page: perPage,
 };
 
 const params = new URLSearchParams(options);
 
-return fetch(`${BASE_URL}api/?${params}`)
-.then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
+try {
+const response = await axios.get(`${BASE_URL}api/?${params}`)
+    console.log(response.data);
+    return response.data;
+  }
+  catch (error) {
+console.error(error);
+throw error;
+  }
 }
+
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { queryFunction } from "./js/pixabay-api";
+import { perPage, queryFunction } from "./js/pixabay-api";
 import { createGalleryMarkup } from "./js/render-functions";
 import "izitoast/dist/css/iziToast.min.css";
 import iziToast from "izitoast";
 
 const form = document.querySelector('form.js-search-form');
 const gallery = document.querySelector('.gallery');
+const btnMore = document.querySelector('button.btn-more');
+let query = null;
+let pageNumber = 1;
 
 const showBox = new SimpleLightbox('.img-box a', { 
     captions: true,
@@ -24,14 +27,16 @@ const showBox = new SimpleLightbox('.img-box a', {
         document.getElementById('loader').style.display = 'none';
     }
 
- function onButtonSubmit (event) {
+ async function onButtonSubmit (event) {
         event.preventDefault();
+        pageNumber = 1;
         showLoader();
     
     const forma = event.currentTarget;
     const {
-        searchValue: { value: query },
+        searchValue: { value},
       } = forma.elements;
+      query = value;
     console.log(query);
    
     if (query === "") {
@@ -46,7 +51,7 @@ const showBox = new SimpleLightbox('.img-box a', {
      
     gallery.innerHTML = "";  
     
-    queryFunction(query)
+    await queryFunction(query)
       .then((photos) => {
         if(!photos.hits || photos.hits.length === 0) {
             iziToast.show({
@@ -60,6 +65,8 @@ const showBox = new SimpleLightbox('.img-box a', {
         console.log(photos);
         gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(photos.hits));
         showBox.refresh();
+        btnMore.style.display = "block";
+        return query;
     })
           
       .catch((error) => console.log(error))
@@ -67,6 +74,47 @@ const showBox = new SimpleLightbox('.img-box a', {
     }
 
 
+    
+    async function btnMoreClick () {
+      
+      try {
+        showLoader();
+        pageNumber += 1;
+    const photos = await queryFunction(query, pageNumber);
+      console.log(pageNumber);
+      let totalPages = Math.ceil(photos.totalHits / perPage)
+      console.log(totalPages);
 
+      if (pageNumber >= totalPages) {
+        btnMore.style.display = "none";
+        iziToast.show({
+          title: "❌",
+          message: `"We're sorry, but you've reached the end of search results."`,
+          position: 'bottomCenter',
+          color: 'blue',
+      })
+      }
+      gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(photos.hits));
+      handleScrollView();
+      showBox.refresh();
+          } catch (error) {
+            console.log(error);
+          } finally { hideLoader();
+          }     
+    }
+
+    function handleScrollView () {
+      const lastArticle = gallery.lastElementChild;
+      const articleHeight = lastArticle.getBoundingClientRect().height;
+      const scrollHeight = articleHeight * 2;
+      
+      window.scrollBy({
+        top: scrollHeight,
+        left: 0,
+        behavior: 'smooth',
+      })
+    }
+
+btnMore.addEventListener('click', btnMoreClick)
 form.addEventListener('submit', onButtonSubmit);
 
